5571042b5c20aa8dbb7779aeff28c7022f2cefa6
Phillip Smith <blueshiftps@gmail.com>
2017-06-28 18:37:39 -0400

    Test edge cases including ES6 arrow functions

--- test/stringify.test.js
+++ test/stringify.test.js
@@ -9,6 +9,9 @@ const regexEqual = function(x, y) {
         (x.source === y.source) && (x.global === y.global) && 
         (x.ignoreCase === y.ignoreCase) && (x.multiline === y.multiline);
 };
+let testFnNoLeadingSpaces = function(){
+    return foo;
+};
 
 describe('lib/stringify.js', function(){
     describe('#stringify(obj, opts)', function(){
@@ -19,19 +22,21 @@ describe('lib/stringify.js', function(){
                 {type: 'undefined', val: undefined, expected: 'undefined'},
                 {type: 'string',    val: 'foo',     expected: '\'foo\''},
                 {type: 'number',    val: 0,         expected: '0'},
+                {type: 'number',    val: -0,        expected: '-0'},
                 {type: 'number',    val: 3.14,      expected: '3.14'},
                 {type: 'boolean',   val: true,      expected: 'true'},
                 {type: 'boolean',   val: false,     expected: 'false'}
             ];
             tests.forEach(function(test) {
                 let output = stringify(test.val);
-                it(`Correctly stringifies type ${test.type} (${test.val})`, function(){
+                it(`Correctly stringifies type ${test.type} (${test.expected})`, function(){
                     assert.equal(test.expected, output);
                 });
-                it(`Stringified ${test.type} ${test.val} evals to the original input`, function(){
+                it(`Stringified ${test.type} ${test.expected} evals to the original input`, function(){
                     assert.equal(test.val, eval(output));
                 });
             });
+            
         });
 
         describe('boxed primitives', function(){
@@ -67,30 +72,71 @@ describe('lib/stringify.js', function(){
         });
 
         describe('functions', function(){
-            let orig = function(a, b){
-                if(a > b) return 1;
-                else if(a === b) return 0;
-                else return -1;
+            let testCases = {
+                simpleFn: {
+                    orig: function(a, b){
+                        if(a > b) return 1;
+                        else if(a === b) return 0;
+                        else return -1;
+                    },
+                    expected: 'function (a, b){'+
+                        '\r\n    if(a > b) return 1;'+
+                        '\r\n    else if(a === b) return 0;'+
+                        '\r\n    else return -1;'+
+                        '\r\n}'
+                },
+                fnNoSpaces: {
+                    orig: testFnNoLeadingSpaces,
+                    expected: 'function (){'+
+                        '\r\n    return foo;'+
+                        '\r\n}'
+                },
+                arrowFn: {
+                    orig: foo => { 'bar' },
+                    expected: 'foo => { \'bar\' }'
+                },
+                arrowFnLong: {
+                    orig: (foo, bar) => {
+                        return 'baz';
+                    },
+                    expected: '(foo, bar) => {'+
+                        '\r\n    return \'baz\';'+
+                        '\r\n}'
+                }
             };
-            let expected = 'function (a, b){'+
-                '\r\n    if(a > b) return 1;'+
-                '\r\n    else if(a === b) return 0;'+
-                '\r\n    else return -1;'+
-                '\r\n}';
-            let output = stringify(orig);
+            testCases.simpleFn.output = stringify(testCases.simpleFn.orig);
+            testCases.fnNoSpaces.output = stringify(testCases.fnNoSpaces.orig);
+            testCases.arrowFn.output = stringify(testCases.arrowFn.orig);
+            testCases.arrowFnLong.output = stringify(testCases.arrowFnLong.orig);
             it('Correctly stringifies functions', function(){
-                assert.equal(output, expected);
+                assert.equal(testCases.simpleFn.output, testCases.simpleFn.expected);
             });
             it('Stringified functions work properly', function(){
-                assert.equal(0, (eval(`(${output})`))(5, 5));
+                assert.equal(0, (eval(`(${testCases.simpleFn.output})`))(5, 5));
+            });
+            it('Does not remove leading spaces unless closing bracket has them', function(){
+                assert.equal(testCases.fnNoSpaces.output, testCases.fnNoSpaces.expected);
+            });
+            it('Correctly stringifies ES6 short arrow functions', function(){
+                assert.equal(testCases.arrowFn.output, testCases.arrowFn.expected);
+            });
+            it('Correctly stringifies ES6 long arrow functions', function(){
+                assert.equal(testCases.arrowFnLong.output, testCases.arrowFnLong.expected);
             });
         });
 
         describe('object', function(){
+            it('Correctly stringifies empty objects', function(){
+                assert.equal('{}', stringify({}));
+            });
             it('Correctly stringifies simple objects', function(){
                 assert.equal('{ a: 1, b: \'foo\', c: null }', stringify({ a: 1, b: 'foo', c: null }));
             });
             
+            it('Preserves odd property names', function(){
+                assert.equal('{ \'0\': 1 }', stringify({ '0': 1 }));
+            });
+
             it('Breaks long objects onto multiple lines', function(){
                 let orig = {
                     stringProp: 'What a long string this is!  Especially if it keeps going...',
@@ -107,11 +153,14 @@ describe('lib/stringify.js', function(){
         });
 
         describe('Arrays', function(){
-            it('Correctly stringifies simple arrays', function(){
+            it('Correctly stringifies empty Arrays', function(){
+                assert.equal('[]', stringify([]));
+            });
+            it('Correctly stringifies simple Arrays', function(){
                 assert.equal('[ 1, \'foo\', null ]', stringify([ 1, 'foo', null ]));
             });
             
-            it('Breaks long arrays onto multiple lines', function(){
+            it('Breaks long Arrays onto multiple lines', function(){
                 let orig = [
                     'What a long string this is!  Especially if it keeps going...',
                     1234567890,
@@ -124,6 +173,10 @@ describe('lib/stringify.js', function(){
                 '\r\n]';
                 assert.equal(stringify(orig), expected);
             });
+        });
+
+        describe('Complex objects', function(){
+
         });
     });
 });
